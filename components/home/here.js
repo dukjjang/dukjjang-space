4d75c65d603df333dbd537f4b4e8a83d117329ef
dukjjang <jhd7292@naver.com>
2022-10-28 09:47:56 +0900

    home 완성

---
+++ components/home/here.js
@@ -0,0 +1,32 @@
+import Animation from './animation';
+export default function Hero() {
+  return (
+    <>
+      <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
+        <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900'>
+          안녕하세요 진현덕입니다!
+          <br className='hidden lg:inline-block' />
+          소통하며 성장하는 신입 개발자입니다
+        </h1>
+        <p className='mb-8 leading-relaxed'>
+          가장 큰 가치는 사람들과 함께 하는데서 만들어진다고 생각합니다.
+          <br />
+          사람들과 좋은 관계를 맺고 소통하며 그안에서 멋진 결과를 만들어내는
+          <br />
+          것을 좋아합니다. 항상 더 나은 서비스를 만들기 위해 고민하고
+          <br />
+          노력하는 신입 개발자입니다. 주도적으로 일 할 수 있는 환경을
+          선호합니다.
+        </p>
+        <div className='flex justify-center'>
+          <button className='inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
+            프로젝트 보러가기
+          </button>
+        </div>
+      </div>
+      <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
+        <Animation />
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Hero from '../components/home/here';
+import Layout from '../components/layout';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout>
       <Head>
         <title>덕짱 포트폴리오</title>
         <meta name='description' content='소통하며 성장하는 신입개발자' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>오늘도 빡코딩</h1>
-    </div>
+
+      <section className='flex min-h-screen flex-col items-center justify-center text-gray-600 body-font'>
+        <div className='container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
+          <Hero />
+        </div>
+      </section>
+    </Layout>
   );
 }
