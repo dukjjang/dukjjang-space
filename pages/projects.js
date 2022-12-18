d0833c98188d0b5252c1105ef9b267e9073f15d5
dukjjang <jhd7292@naver.com>
2022-12-19 08:33:11 +0900

    feat: project 다크모드 배경화면 추가

--- pages/projects.js
+++ pages/projects.js
@@ -10,7 +10,7 @@ export default function Projects() {
         <meta name="projects" content="프로젝트" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="relative bg-[#EEF1F5] w-full h-[1000px]">
+      <main className="relative bg-[#EEF1F5] dark:bg-[#192129] w-full h-[1000px]">
         <h1 className="absolute w-full text-center top-[-320px] font-bold text-4xl ">
           안녕하세요 진현덕입니다!
         </h1>
