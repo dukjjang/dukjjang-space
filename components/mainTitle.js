2631e0b4077b3fd90480321184faf9ea411c49a1
dukjjang <jhd7292@naver.com>
2022-12-19 08:25:43 +0900

    refactor: svg theme 동적 스타일링 리팩토링

--- components/mainTitle.js
+++ components/mainTitle.js
@@ -3,9 +3,7 @@ import Link from "next/link";
 const MainTitle = () => {
   return (
     <Link href="/">
-      <span className=" text-[#4333FF] dark:text-[#00df9a] text-3xl font-bold">
-        Jin Hyunduk
-      </span>
+      <span className=" text-[#00df9a] text-3xl font-bold">Jin Hyunduk</span>
     </Link>
   );
 };
