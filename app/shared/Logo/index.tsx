6aa3ff25245e18fe6ccddb8e88e34fb78e25ea2d
dukjjang <jhd7292@naver.com>
2023-01-20 20:36:03 +0900

    design: 로고 아이콘 마진 추가

--- app/shared/Logo/index.tsx
+++ app/shared/Logo/index.tsx
@@ -4,7 +4,8 @@ const Logo = () => {
   return (
     <Link href="/">
       <div className="relative text-slate-800 dark:text-white text-xl  md:text-3xl font-bold">
-        <span className="dark:text-[#C3FA07] ">👨🏻‍💻D</span>uk
+        <span className="mr-2">👨🏻‍💻</span>
+        <span className="dark:text-[#C3FA07] ">D</span>uk
         <span className="dark:text-[#C3FA07] ">jj</span>a
         <span className="dark:text-[#C3FA07] ">n</span>g
       </div>
