bd75b4974a0fad61344b2fca600292a908b8050c
dukjjang <jhd7292@naver.com>
2023-01-20 20:02:45 +0900

    design: 로고 수정

--- app/shared/Logo/index.tsx
+++ app/shared/Logo/index.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/">
-      <span className="relative text-black dark:text-white text-xl  md:text-3xl font-bold">
-        <span className="dark:text-[#C3FA07]">D</span>uk
-        <span className="dark:text-[#C3FA07]">jj</span>a
-        <span className="dark:text-[#C3FA07]">n</span>g
+      <span className="relative text-slate-800 dark:text-white text-xl  md:text-3xl font-bold">
+        <span className="dark:text-[#C3FA07] ">D</span>uk
+        <span className="dark:text-[#C3FA07] ">jj</span>a
+        <span className="dark:text-[#C3FA07] ">n</span>g
         <div className="dark:invisible"></div>
       </span>
     </Link>
