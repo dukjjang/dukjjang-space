6f78573ef1fa03511b75705566175add683a7593
dukjjang <jhd7292@naver.com>
2023-01-20 20:34:43 +0900

    design: 로고 아이콘 변경

--- app/shared/Logo/index.tsx
+++ app/shared/Logo/index.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <div className="relative text-slate-800 dark:text-white text-xl  md:text-3xl font-bold">
-        <span className="dark:text-[#C3FA07] ">🙋🏻‍♂️D</span>uk
+        <span className="dark:text-[#C3FA07] ">👨🏻‍💻D</span>uk
         <span className="dark:text-[#C3FA07] ">jj</span>a
         <span className="dark:text-[#C3FA07] ">n</span>g
       </div>
