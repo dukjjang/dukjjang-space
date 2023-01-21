0044dfebd21c2696c93984578754b8fe0bb958d9
dukjjang <jhd7292@naver.com>
2023-01-21 19:17:52 +0900

    design: body 배경 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body className="ts-color bg-primary">
+      <body className="ts-color ">
         <Providers>
           <Header />
           {children}
