ec4bfa2bcbc4e3e9c2ce33e01f7311cd2e499e3d
dukjjang <jhd7292@naver.com>
2023-01-26 00:12:10 +0900

    fix: touch 로직 수정

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -38,40 +38,35 @@ const Header = () => {
   };
 
   useEffect(() => {
-    // window.scroll(0, 0);
+    window.scroll(0, 0);
 
     window.addEventListener("scroll", () => {
       setScrollY(window.scrollY);
     });
 
-    console.log("스크롤 들어옴", scrollY);
     return window.removeEventListener("scroll", () => {});
-  }, [scrollY]);
-  console.log("스크롤 ", scrollY);
+  }, []);
 
   const handleDragStart = (e) => {
     setGrab(true);
   };
 
   const handleTouchStart = (e) => {
-    const target = e.target as HTMLElement;
+    console.log("도큐먼트 터치 스타트");
 
-    if (target.hasAttribute("draggable")) {
-      setPosition({
-        x: dragRef.current?.offsetLeft,
-        y: dragRef.current?.offsetTop,
-        dx: dragRef.current?.offsetLeft,
-        dy: dragRef.current?.offsetTop,
-      });
-
-      dragRef.current.style.position = "absolute";
+    if (!e.target.hasAttribute("draggable")) {
+      console.log("도큐먼트 드래거블 아님");
+      return;
     }
-    document.body.style.overflow = "hidden";
-    console.log("도큐멘트 스타트");
+
+    // document.body.style.overflow = "hidden";
+
+    // dragRef.current.style.position = "absolute";
   };
 
   const handleTouchMove = (e) => {
     const touch = e.touches[0];
+    // if (e.cancelable) e.preventDefault();
 
     if (
       touch.pageX < 0 + dragRef.current!.offsetWidth / 2 ||
@@ -111,29 +106,36 @@ const Header = () => {
     setIsDragging(false);
     setGrab(false);
 
+    dragRef.current.style.removeProperty("position");
     setPosition({ ...position, y: position.dy, x: position.dx });
-    dragRef.current.style.position = "";
-    document.body.style.removeProperty("overflow");
 
-    console.log("엔드");
+    document.body.style.removeProperty("overflow");
+    document.body.style.removeProperty("position");
   };
 
   useEffect(() => {
-    setPosition({
-      ...position,
-      dx: dragRef.current.offsetLeft,
-      dy: dragRef.current.offsetTop,
-    });
-    document.getElementById("nav").addEventListener(
+    document.addEventListener("touchstart", handleTouchStart);
+
+    document.getElementById("header").addEventListener(
       "touchstart",
       (e) => {
+        console.log("헤더에서 터치");
+
         const target = e.target as HTMLElement;
         if (target.hasAttribute("draggable")) {
           e.preventDefault();
-          document.addEventListener("touchstart", handleTouchStart);
-          console.log("헤더에서 터치");
+
+          setPosition({
+            x: dragRef.current?.offsetLeft,
+            y: dragRef.current?.offsetTop,
+            dx: dragRef.current?.offsetLeft,
+            dy: dragRef.current?.offsetTop,
+          });
+
+          // document.body.style.overflow = "hidden";
           console.log("헤더에서 드래거블임");
-        }
+          dragRef.current.style.position = "absolute";
+        } else return;
       },
       { passive: false }
     );
@@ -141,6 +143,7 @@ const Header = () => {
 
   return (
     <motion.header
+      id="header"
       {...animation}
       className={`${!isHomePage && "sticky"} top-0 w-full ${
         isHomePage ? "bg-primary ts-color" : "bg-transparent"
@@ -148,17 +151,14 @@ const Header = () => {
     >
       <div className=" text-background mx-auto flex py-8 px-5 md:px-20 lg:px-64 w-full items-center">
         <Logo />
-        <nav
-          id={"nav"}
-          className=" text-background h-14 gap-3 md:gap-8 font-normal text-[16px] ml-auto flex items-center justify-center"
-        >
+        <nav className=" text-background h-14 gap-3 md:gap-8 font-normal text-[16px] ml-auto flex items-center justify-center">
           <Image
             style={{ top: position.y, left: position.x }}
             ref={dragRef}
             id={"wizard"}
+            onTouchMove={handleTouchMove}
             onTouchEnd={handleDragEnd}
             onDragStart={handleDragStart}
-            onTouchMove={handleTouchMove}
             onDragEnd={handleDragEnd}
             draggable
             width={35}
