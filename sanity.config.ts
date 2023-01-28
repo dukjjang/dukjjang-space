93369efdf23d6f6803f50bb2356a992f302d5328
dukjjang <jhd7292@naver.com>
2023-01-28 22:57:43 +0900

    feat: sanity deploy trigger 추가

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
+import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -13,7 +14,7 @@ export default defineConfig({
   projectId,
   dataset,
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), vercelDeployTool()],
 
   schema: {
     types: schemaTypes,
