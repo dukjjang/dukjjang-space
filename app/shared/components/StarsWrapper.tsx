acf140f66c093c0e1bf3e755d32fa9afe5c6bd18
dukjjang <jhd7292@naver.com>
2023-01-31 21:05:34 +0900

    feat: useSpring sun

--- app/shared/components/StarsWrapper.tsx
+++ app/shared/components/StarsWrapper.tsx
@@ -1,7 +1,9 @@
+import { usePathname } from "next/navigation";
 import { RefObject, useEffect, useState } from "react";
 import Star from "./Star";
 
 const StarsWrapper = ({ headerRef }: { headerRef: RefObject<HTMLElement> }) => {
+  const pathName = usePathname();
   const [stars, setStars] = useState([]);
   const windowWidth = window.innerWidth ?? document.body.offsetWidth;
 
@@ -10,14 +12,16 @@ const StarsWrapper = ({ headerRef }: { headerRef: RefObject<HTMLElement> }) => {
       Array.from(new Array(20), (_) => {
         return {
           x: (Math.random() * headerRef.current.offsetWidth).toFixed(0),
-          y: ((Math.random() * headerRef.current.offsetHeight) / 2).toFixed(0),
+          y: pathName.match("writing")
+            ? (Math.random() * headerRef.current.offsetHeight).toFixed(0)
+            : ((Math.random() * headerRef.current.offsetHeight) / 2).toFixed(0),
         };
       })
     );
   }, [windowWidth]);
 
   return (
-    <div className="hidden dark:block absolute left-0 top-0 right-0 h-full w-full bg-transparent">
+    <div className=" hidden dark:block absolute left-0 top-0 right-0 h-full w-full bg-transparent">
       {stars.map((star) => (
         <Star key={crypto.randomUUID()} x={Number(star.x)} y={Number(star.y)} />
       ))}
--- app/shared/components/SunMoon.tsx
+++ app/shared/components/SunMoon.tsx
@@ -1,5 +1,7 @@
-import { motion } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
 import { usePathname } from "next/navigation";
+import { useSpring } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   showSideTaps: boolean;
@@ -8,18 +10,31 @@ type Props = {
 
 const SunMoon = ({ showSideTaps, sunOrMoon }: Props) => {
   const isHomePage = usePathname().length < 1;
+  const sunMoonRef = useRef<HTMLDivElement>(null);
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  useSpring(x, { stiffness: 1000, damping: 10 });
+  useSpring(y, { stiffness: 1000, damping: 10 });
+
 
   return (
     <motion.div
+      ref={sunMoonRef}
+      drag
+      dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+      dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }}
+      dragElastic={1}
       animate={{
         translateY: [0, 10, 0],
       }}
+      style={{ x, y }}
       transition={{ repeat: showSideTaps ? 0 : Infinity, duration: 3 }}
       className={` ${sunOrMoon} ${
         sunOrMoon === "sun" ? "block dark:hidden" : "hidden dark:block"
       } cursor-pointer z-30 will-change-transform w-10 h-10 ${
         isHomePage && "md:w-12 md:h-12"
-      } rounded-full absolute top-0 left-0 `}
+      } rounded-full absolute top-0 left-0  `}
     />
   );
 };
