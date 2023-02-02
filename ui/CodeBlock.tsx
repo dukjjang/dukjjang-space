f359c3e3f8eda9b7b1c8c53386a805b4634c4dc4
dukjjang <jhd7292@naver.com>
2023-02-02 23:43:02 +0900

    feat: code block copy

--- ui/CodeBlock.tsx
+++ ui/CodeBlock.tsx
@@ -1,13 +1,49 @@
 "use client";
 
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { materialOceanic } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { useState } from "react";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { xonokai } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const CodeBlock = ({ value }: any) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyCode = async () => {
+    try {
+      await navigator.clipboard.writeText(value.code);
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 3000);
+    } catch (err) {
+      console.error("faild copy", err);
+      setIsCopied(false);
+    }
+  };
+
   return (
-    <SyntaxHighlighter language={value.language} style={materialOceanic}>
-      {value.code}
-    </SyntaxHighlighter>
+    <div className=" bg-[#303136] dark:bg-[#303136]/70  h-fit p-3 rounded-md w-full flex flex-col justify-center">
+      <button
+        type="button"
+        className="self-end mr-5 px-2  py-1 bg-transparent  "
+        onClick={handleCopyCode}
+      >
+        <h4 className="text-gradient text-md">
+          {isCopied ? "copied" : "copy"}
+        </h4>
+      </button>
+      <SyntaxHighlighter
+        customStyle={{
+          backgroundColor: "transparent",
+          borderRadius: "10px",
+          padding: "20px",
+          border: "0px",
+        }}
+        language={value.language}
+        style={xonokai}
+      >
+        {value.code}
+      </SyntaxHighlighter>
+    </div>
   );
 };
 
