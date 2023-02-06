13ee0b2be956c5a3f458d35c594c1da74759ee52
dukjjang <jhd7292@naver.com>
2023-02-07 07:25:55 +0900

    feat: logo id 추가

--- ui/Logo.tsx
+++ ui/Logo.tsx
@@ -1,6 +1,7 @@
 const Logo = () => {
   return (
     <h1
+      id="logo"
       className="font-extrabold text-transparent dark:text-transparent text-[24px] md:text-4xl
         bg-clip-text bg-gradient-to-r from-cyan-400 to-green-400 dark:from-green-300 dark:to-blue-600"
     >
