7f2069ab7fc854b4c8c75971e698645ab8838899
dukjjang <jhd7292@naver.com>
2023-02-09 18:54:41 +0900

    fix: sound toggle px

--- ui/SoundToggleButton.tsx
+++ ui/SoundToggleButton.tsx
@@ -20,7 +20,7 @@ const SoundToggleButton = () => {
   return (
     <div
       className="text-orange-400 dark:text-yellow-400 md:hover:scale-150 cursor-pointer 
-       transition-all ease-in-out md:active:translate-y-3"
+       transition-all ease-in-out md:active:translate-y-3 px-1"
       onClick={toggleSoundSetting}
     >
       {sound ? <SlVolume2 size={25} /> : <SlVolumeOff size={25} />}
